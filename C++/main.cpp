ca14198d4217c93bfb391de587b85e919ac06f93
mohdadnan786 <60983364+mohdadnan786@users.noreply.github.com>
2023-10-19 23:13:20 +0530

    String
    Hactoberfest 2023

---
+++ C++/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  change character of string
+//
+//  Created by Mohd  Adnan on 07/09/21.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    string mystring = "ADNAN";
+    mystring[2]='a';
+    cout << mystring;
+    return 0;
+}
